31fcf1473aec8b4314732a90ef3daa3b8150840a
jeffcodes <jeffcodes13@gmail.com>
2026-03-16 21:04:25 +0530

    Enable automatic DB migrations

--- Services/IdentityService/Identity.API/Program.cs
+++ Services/IdentityService/Identity.API/Program.cs
@@ -90,6 +90,12 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<IdentityDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseMiddleware<Identity.API.Middleware.ExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
